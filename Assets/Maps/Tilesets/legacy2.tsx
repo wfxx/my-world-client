219730f58afc2895bd3d9d988fbcaa18aa2a7f10
xiabo <wfxxch@gmail.com>
2023-01-22 11:45:24 +0800

    * 修改一波代码

---
+++ Assets/Maps/Tilesets/legacy2.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="legacy" tilewidth="16" tileheight="16" tilecount="4810" columns="74">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <image source="../Images/legacy_atlas_down.png" width="1184" height="1040"/>
+</tileset>
